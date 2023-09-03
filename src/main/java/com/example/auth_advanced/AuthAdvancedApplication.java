0d987130c82a093fe9b59a3528ffdfc52756764f
saranvorn111 <Saranvornprogrammer@gmail.com>
2023-09-03 21:09:43 +0700

    Resource_auth

---
+++ src/main/java/com/example/auth_advanced/AuthAdvancedApplication.java
@@ -0,0 +1,13 @@
+package com.example.auth_advanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthAdvancedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthAdvancedApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/auth_advanced/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.example.auth_advanced.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+import org.springframework.security.web.SecurityFilterChain;
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(authorize ->
+
+                        authorize.anyRequest().authenticated())
+
+                .oauth2ResourceServer(oauth2 ->
+
+                        oauth2.jwt((Customizer.withDefaults())));
+
+        return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(){
+
+       NimbusJwtDecoder nimbusJwtDecoder = NimbusJwtDecoder.withJwkSetUri("" +
+               "http://localhost:8080/oauth2/jwks").build();
+
+        return nimbusJwtDecoder;
+    }
+}
